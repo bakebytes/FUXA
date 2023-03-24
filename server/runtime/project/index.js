200aa1051b835e385685c8bcea70d72f7d04f90e
unocelli <unocelli@gmail.com>
2023-03-24 11:00:28 +0100

    restore issue save daq settings

--- server/runtime/project/index.js
+++ server/runtime/project/index.js
@@ -907,7 +907,6 @@ function _mergeDefaultConfig() {
                                     continue;
                                 }
                                 try {
-                                    logger.info(storedTag);
                                     var tagToAdd = new Tag(tag.id);
                                     if (tag.name) tagToAdd.name = tag.name;
                                     if (tag.label) tagToAdd.label = tag.label;
